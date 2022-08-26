d0f0cf06f42aff0688f8e192194323c23c3fb1a1
Matthew <matthewk8819@gmail.com>
2022-08-26 08:43:12 -0700

    Created ATM, tester,

---
+++ src/ATM.java
@@ -0,0 +1,8 @@
+
+public class ATM {
+	private int accountID;
+	
+	public ATM (String name, int ID) {
+		
+	}
+}
---
+++ src/ATMTester.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class ATMTester {
+	public static void main (String [] args) {
+		
+	}
+}
