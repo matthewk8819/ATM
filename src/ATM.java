b988a016eff79bdce60c525f12dd1626e2aa3caf
Matthew <matthewk8819@gmail.com>
2022-08-30 12:08:46 -0700

    First draft of the atm class, account class with all methods

--- src/ATM.java
+++ src/ATM.java
@@ -1,8 +1,44 @@
+import java.util.*;
+
 
 public class ATM {
-	private int accountID;
 	
-	public ATM (String name, int ID) {
-		//matthew is a mango
+	private HashMap<Integer,account> allAccounts = new HashMap<Integer,account>();
+	
+	public ATM () {
+		
 	}
+	
+	public void openAccount(int id) {
+		account newaccount = new account(id);
+		allAccounts.put(id, newaccount);
+	}
+	
+	public void openAccount(int accountID, double ammount) {
+		account newaccount = new account(accountID, ammount);
+		allAccounts.put(accountID, newaccount);
+	}
+	
+	public void closeAccount (int accountID) {
+		allAccounts.remove(accountID);
+	}
+	
+	public double checkBalance (int accountID) {
+		return allAccounts.get(accountID).getBalance();
+	} 
+	
+	private boolean isAccount (int id) {
+		return allAccounts.containsKey(id);
+	}
+	
+	public boolean withdrawMoney (int accountID, double amount) {
+		account acc = allAccounts.get(accountID);
+		if (acc.getBalance()<amount) return false;
+		else {
+			acc.setBalance(acc.getBalance()-amount);
+			return true;
+		}
+	}
+	
+	
 }
---
+++ src/account.java
@@ -0,0 +1,31 @@
+
+public class account {
+	private int id;
+	
+	private double balance; 
+	
+	public account(int id) {
+		this.id = id;
+	}
+	
+	public account(int id, double balance) {
+		this.id = id;
+		this.balance = balance;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public int getID () {
+		return id;
+	}
+	
+	public double getBalance () {
+		return balance;
+	}
+}
